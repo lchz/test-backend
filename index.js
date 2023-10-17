dc9233d0add5e04c4bc592410b5a4bf32a6b9a83
lchz <lauraz940829@hotmail.com>
2023-10-17 21:09:08 +0300

    add routes

--- index.js
+++ index.js
@@ -5,21 +5,6 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
 
-    
-
-// let chapters = [
-//     {
-//       id: 1,
-//       content: "第 1 章<br><br>夏清清沒有想到，自己身處的世界原來是一本小說。<br><br>他在自己的十九歲生日宴上，被剛找回來的私生子哥哥夏缺推進池塘裏。<br><br>",
-//       important: true
-//     },
-//     {
-//       id: 2,
-//       content: "第 2 章<br><br>夏",
-//       important: false
-//     },
-//   ]
-
 
 
 app.get('/', (request, response) => {
