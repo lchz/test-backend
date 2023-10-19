9ff44092a7a2e1c558bdb007290e1197989d679c
lchz <lauraz940829@hotmail.com>
2023-10-19 21:51:13 +0300

    Modified frontend using Router

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const data = require('./sample.json')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-let chapters = [
-  {
-    "id": 0,
-    "title": "backend 第25章",
-    "content": "第 25 章<br><br>“轰”——<br><br>惊雷落下，猛然将夏清清从夢魇中震醒。<br>"
 
-  },
-  {
-    "id": 1,
-    "title": "backend 第26章",
-    "content": "第 26 章 <br>"
-
-  },
-
-]
+const chapters = data.chapters
 
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello Backend</h1>')
+  response.send('<h1>Hello Backend</h1>')
 })
 
 app.get('/api/chapters', (request, response) => {
-  // response.send('<h1>Hello Backend</h1>')
   response.send(chapters)
 })
 
 app.get('/api/chapters/:id', (request, response) => {
     const id = Number(request.params.id)
+    console.log('Backend ID:', id)
+
+    if (id >= chapters.length || id < 0) {
+        console.log('This is undefined')
+      return response.json({error: 'Entry error'})
+    }
+
     const ch = chapters.find(c => c.id === id)
-    response.json(ch)
 
     console.log(response.get('Content-Type'))
-    console.log(ch)
+    console.log('chapter title:', ch.title)
+
+    return response.json(ch)
 })
 
-app.post('/api/chapters', (req, res) => {
-  const newChap = req.body
+// app.post('/api/chapters', (req, res) => {
+//   const newChap = req.body
 
-  newChap.id = chapters.length
-  chapters = chapters.concat(newChap)
+//   newChap.id = chapters.length
+//   chapters = chapters.concat(newChap)
 
-  res.json(newChap)
-})
+//   res.json(newChap)
+// })
 
 
 
